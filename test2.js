1431ef46274e9dbbf28fb139c39d44d1c207544c
mochimochitaro <167455641+mochimochitaro@users.noreply.github.com>
2024-04-19 10:37:55 +0900

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,6 +1,16 @@
-protected void doGet(HttpServletRequest request, HttpServletResponse resp) {
+protected void doGet(HttpServletRequest request, HttpServletResponse resp) throws IOException {
+    resp.setContentType("text/html");
+    resp.setCharacterEncoding("UTF-8");
+    
     String firstName = request.getParameter("firstName");
-    resp.getWriter().append("<div>");
-    resp.getWriter().append("Search for " + firstName);
-    resp.getWriter().append("</div>");
+    PrintWriter out = resp.getWriter();
+    
+    out.append("<html>");
+    out.append("<head><title>Search Result</title></head>");
+    out.append("<body>");
+    out.append("<div>");
+    out.append("Search for " + firstName);
+    out.append("</div>");
+    out.append("</body>");
+    out.append("</html>");
 }
